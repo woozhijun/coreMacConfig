5c445fe7de8440a576c4c66ec5f5b26c3cd9b5db
wuzhijun <wuzhijun@mobike.com>
2018-11-16 19:00:33 +0800

    add alias

--- alias.sh
+++ alias.sh
@@ -1,1 +1,20 @@
-#!/bin/bash
+
+alias ll='ls -alFG'
+alias la='ls -A'
+alias ls='ls -G'
+
+# java
+alias java='java -Dfile.encoding=UTF-8'
+alias javac='javac -J-Dfile.encoding=UTF-8 -encoding UTF-8'
+alias mpk='mvn -U -Dmaven.test.skip=true clean package'
+
+# git
+alias ga='git add'
+alias gb='git branch'
+alias gc='git commit'
+alias gcb='git checkout -b'
+alias gcm='git commit -m'
+alias gd='git diff'
+alias gf='git fetch'
+alias gg='git lg'
+alias gm='git merge'
